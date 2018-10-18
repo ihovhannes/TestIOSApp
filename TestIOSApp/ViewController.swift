cf6284d2247c1a9318d23f8f2a45a65ba4598372
Hovhannes Sukiasyan <hovhannessukiasyan@macbook-pro-hovhannes.local>
2018-10-18 22:29:19 +0700

    Initial Commit

---
+++ TestIOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestIOSApp
+//
+//  Created by Hovhannes Sukiasyan on 18.10.2018.
+//  Copyright © 2018 Hovhannes Sukiasyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
